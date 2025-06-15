e49cec3cb5ecaba5e3956d6ca174b09bf8ca1a95
Klausbdl <lucas_cmattos@hotmail.com.br>
2025-06-15 22:32:21 +0100

    Try having everything in the same path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-fetch('../pages/header.html')
+fetch('header.html')
     .then(res => res.text())
     .then(data => document.getElementById('header').innerHTML = data);
 
-fetch('../pages/footer.html')
+fetch('footer.html')
     .then(res => res.text())
     .then(data => document.getElementById('footer').innerHTML = data);
